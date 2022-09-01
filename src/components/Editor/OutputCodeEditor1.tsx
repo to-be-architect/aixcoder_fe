fae812eb17116bc773efe3601cf037bb3eb80e93
chenguangjian.jk <chenguangjian.jk@bytedance.com>
2022-09-02 01:27:25 +0800

    // aiXCoder Output 3:

--- src/components/Editor/OutputCodeEditor1.tsx
+++ src/components/Editor/OutputCodeEditor1.tsx
@@ -9,7 +9,35 @@ const editorStyle = {
 
 function OutputCodeEditor1() {
   const date = new Date();
-  const code = `// 根据函数名智能生成代码 ${date}`;
+  const code = `// 根据函数名智能生成代码 ${date}
+// aiXCoder Output 3:
+func MergeTwoBinaryTree(t1 TreeNode, t2 TreeNode) (TreeNode, error)  {
+\tif t1 == nil {
+\t\treturn nil, nil
+\t}
+\tif t2 == nil {
+\t\treturn t1, nil
+\t}
+\tvar t1Node, t2Node *TreeNode
+\tif t1.Left!= nil {
+\t\tt1Node = t1.Left
+\t\tt1.Left = nil
+\t}
+\tif t2.Right!= nil {
+\t\tt2Node = t2.Right
+\t\tt2.Right = nil
+\t}
+\tif t1Node == nil {
+\t\treturn t2Node, nil
+\t}
+\tif t2Node == nil {
+\t\treturn t1Node, nil
+\t}
+\tt1Node.Right = MergeTwoBinaryTree(t1Node.Right, t2Node.Right)
+\tt1Node.Left = MergeTwoBinaryTree(t1Node.Left, t2Node.Left)
+\treturn t1Node, nil
+}
+  `;
 
   return (
     <div style={editorStyle}>
--- src/components/Editor/OutputCodeEditor3.tsx
+++ src/components/Editor/OutputCodeEditor3.tsx
@@ -9,7 +9,45 @@ const editorStyle = {
 
 function OutputCodeEditor3() {
   const date = new Date();
-  const code = `# 根据自然语言智能生成代码 ${date}`;
+  const code = `# 根据自然语言智能生成代码 ${date}
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def mergeTrees(self, root1: TreeNode, root2: TreeNode) -> TreeNode:
+        if not root1:
+            return root2
+        if not root2:
+            return root1
+
+        root1.val += root2.val
+        root1.left = self.mergeTrees(root1.left, root2.left)
+        root1.right = self.mergeTrees(root1.right, root2.right)
+
+        return root1
+
+root1 = TreeNode(1)
+root1.left = TreeNode(2)
+root1.right = TreeNode(3)
+root1.left.left = TreeNode(4)
+root1.left.right = TreeNode(5)
+root1.right.left = TreeNode(6)
+root1.right.right = TreeNode(7)
+
+root2 = TreeNode(1)
+root2.left = TreeNode(2)
+root2.right = TreeNode(3)
+root2.left.left = TreeNode(4)
+root2.left.right = TreeNode(5)
+root2.right.left = TreeNode(6)
+root2.right.right = TreeNode(7)
+
+result = Solution().mergeTrees(root1, root2)
+print(result.val) 
+  `;
 
   return (
     <div style={editorStyle}>
